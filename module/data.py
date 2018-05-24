322abf86c68b42e89a091fee466441a7df368b6c
Renan <renanferreiradelima98@live.com>
2018-05-24 18:20:49 -0300

    Adição do módulo scikit

--- module/data.py
+++ module/data.py
@@ -1,37 +1,32 @@
-import pandas as pd
+from pandas import read_csv
+from platform import system
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
-import platform
 
 class Data:
 
     def __init__(self, path):
 
         #Lê arquivo disponibilizado pelo Avito
-        if platform.system() == 'Windows': self.__data = pd.read_csv(path, encoding='latin1');
-        else: self.__data = pd.read_csv(path, encoding='utf8');
+        if system() == 'Windows': self.__data = read_csv(path, encoding='latin1');
+        else: self.__data = read_csv(path, encoding='utf8');
         print('Dados carregados');
+
+        #Stop words russas
+        stopWords = open('file/stopwords.txt', encoding='utf8').read().split('\n');
         
         #Obetendo cabeçalhos
         heads = self.__data.columns.values[(self.__data.columns.values !='activation_date') & (self.__data.dtypes == 'object') ];
-
-        #words = set();
         
         #Stop words russas
         stopWords = open('file/stopwords.txt', encoding='utf8').read().split('\n');
         print("Stop Words carregadas");
-
+        
         #Pré processa os dados
         for head in heads:
-                
-            #Passa as palavras para minúsculo
-            self.__data[head] = self.__data[head].str.lower();
-                
-            #Remove stop words
-            self.__data[head].replace(stopWords, '', inplace=True);
-
-            #Obtém as palavras que aparecem em cada cabeçalho
-            #self.__data[head].str.split(' ').apply(words.update);
-                
+        
+            tfidf = TfidfVectorizer(sublinear_tf=True, stop_words=stopWords, max_df=0.8);
+            print(tfidf.fit_transform(self.__data[head][self.__data[head].notnull()].tolist()));
             print('Pré-processou', head);
 
         print('Pré-processo concluído');
@@ -49,4 +44,3 @@ class Data:
     def getClassification(self, classification, value): return np.where(self.__data[classification].unique() == value)[0][0];
 
 d = Data('file/train.csv');
-#d.read();
