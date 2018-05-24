321d4a9f104b88c3ade1968e024cf61c6bc66372
Renan <renanferreiradelima98@live.com>
2018-05-24 14:37:44 -0300

    Otimização do leitor dos dados

--- module/data.py
+++ module/data.py
@@ -1,25 +1,40 @@
 import pandas as pd
 import numpy as np
+import platform
 
 class Data:
 
     def __init__(self, path):
-        self.__data = pd.read_csv(path, encoding='latin-1');
 
-        #Stop Words russas
-        stopWords = open('file/stopwords-ru.txt', encoding='latin-1').read().split('\n');
-
+        #Lê arquivo disponibilizado pelo Avito
+        if platform.system() == 'Windows': self.__data = pd.read_csv(path, encoding='latin1');
+        else: self.__data = pd.read_csv(path, encoding='utf8');
+        print('Dados carregados');
+        
         #Obetendo cabeçalhos
-        for head in self.__data.columns.values:
+        heads = self.__data.columns.values[(self.__data.columns.values !='activation_date') & (self.__data.dtypes == 'object') ];
 
-            #Verifica se os dados do cabeçalho são do tipo string
-            if self.__data[head].dtype == 'O':
+        #words = set();
+        
+        #Stop words russas
+        stopWords = open('file/stopwords.txt', encoding='utf8').read().split('\n');
+        print("Stop Words carregadas");
 
-                #Passa as palavras para minúsculo
-                self.__data[head] = self.__data[head].str.lower();
+        #Pré processa os dados
+        for head in heads:
+                
+            #Passa as palavras para minúsculo
+            self.__data[head] = self.__data[head].str.lower();
+                
+            #Remove stop words
+            self.__data[head].replace(stopWords, '', inplace=True);
 
-                #Remove stop words
-                self.__data[head].replace(stopWords, '', inplace=True);
+            #Obtém as palavras que aparecem em cada cabeçalho
+            #self.__data[head].str.split(' ').apply(words.update);
+                
+            print('Pré-processou', head);
+
+        print('Pré-processo concluído');
 
     #Exibe os dados
     def read(self): print(self.__data);
@@ -33,5 +48,5 @@ class Data:
     #Obtém a classificação
     def getClassification(self, classification, value): return np.where(self.__data[classification].unique() == value)[0][0];
 
-data = Data('file/train.csv');
-data.read();
+d = Data('file/train.csv');
+#d.read();
